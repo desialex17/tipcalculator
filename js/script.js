f123667e83d62080f2ea7fd68465701e15193ed6
Desiree <cexuser@Oreo.local>
2015-03-16 20:45:51 -0400

    added mad

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
 //add script here
+
+function tipCalculate (){
+
+var totalBill = document.getElementById('bill').value;
+console.log(totalBill);
+
+var percBtns = document.getElementsByClassName('tip-btn');
+console.log(percBtns);
+
+for ( var i=0; i<percBtns.length; i++)
+{
+
+	if(percBtns[i].checked) {
+		var tipPercent = percBtns[i].value; 
+	}
+}
+
+
+var tipTotal = totalBill * tipPercent;
+
+var tipBox = document.getElementById('tipamount');
+
+tipBox.innerHTML = '$' + tipTotal;
+
+}

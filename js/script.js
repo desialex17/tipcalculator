429fcde91a791f05dce94385251430a02a7fa37f
Desiree <cexuser@Oreo.local>
2015-03-23 19:25:18 -0400

    added stuff

--- js/script.js
+++ js/script.js
@@ -10,17 +10,39 @@ console.log(percBtns);
 
 for ( var i=0; i<percBtns.length; i++)
 {
-
 	if(percBtns[i].checked) {
 		var tipPercent = percBtns[i].value; 
 	}
 }
 
-
+if (!isNaN(totalBill)){
+	
 var tipTotal = totalBill * tipPercent;
 
 var tipBox = document.getElementById('tipamount');
 
-tipBox.innerHTML = '$' + tipTotal;
+tipBox.innerHTML = '$' + tipTotal;}
 
+else{
+alert('Please enter numbers only');
+resetPage();
+}}
+
+function resetPage(){
+  document.getElementById('bill').value = '';
+
+  var percBtns = document.getElementsByClassName('tip-btn');
+  for(var i=0; i < percBtns.length; i++){
+    percBtns[i].checked = false;
+  }
+
+  var tipBox = document.getElementById('tipamount');
+  tipBox.innerHTML = '$0.00';
 }
+
+
+
+
+
+
+
